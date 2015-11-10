02ad4d3f5a165e574df391da36d95fe7a9c4fb4a
TaXxX <henrry_el_master@hotmail.com>
2015-11-10 18:29:05 +0100

    new server

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@ module.exports = {
 
 	// the database url to connect
 	//url : 'mongodb://localhost/'+'lightpath'
-	url : 'mongodb://104.214.33.26/'+'lightpath'
+	//url : 'mongodb://104.214.33.26/'+'lightpath'
+	url : 'mongodb://23.102.155.93/'+'lightpath'
 }
